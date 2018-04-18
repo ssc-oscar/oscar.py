d186ff7fcc34f2c172f4ece3b25186ce1f5f0ab6
Marat <user2589@users.noreply.github.com>
2018-04-17 23:41:48 -0400

    basic object hierarchy

--- docs/DataFormat.md
+++ docs/DataFormat.md
@@ -2,15 +2,81 @@
 
 ## Git objects
 
-All git ogjects are stored basically the same way.
-Each git object is uniquely identified by its SHA1 hash.
-First, you need to convert this hash into a record number:
+### Sequential access:
 
-- Obtain a record number from da4:/fast1/All.sha1/sha1.{commit,tree,tag,blob}_{0..127}.tch
-  Use proper object type (blob, commit, tree or tag) and index (first 8 bits of sha-1 hash)
-  to refer to the right Tokyo Cabinet file
+Read an index line from /data/All.blobs/{commit,blob,tag,tree}_{key}.idx
+to get offset and length. 
+Note that the key, a 1..127 uint, is 7 least significant bits of the
+first byte of sha, and not 7 msb. 
+So, to get the key, use `sha[0] & 7f`, NOT bit shift.
 
-### Blobs
+Content of .idx files for commits, trees and tags:
+
+    id, offset, length, object sha
+
+e.g.:
+
+    0;0;267;80b4ca99f8605903d8ac6bd921ebedfdfecdd660
+    1;267;185;0017b852ce7b49225c5a797b3d4221d363c0acdd
+    2;452;167;0054bab7302b386ddf2350a3fb2db08d59e125e1
+    3;619;235;8028315640bac6eae17297270d4ee1892abf6add
 
 
-**How**: record contains just a number
+Blobs:
+
+    id, offset, length, ??int, blob sha, ??sha, ??int
+
+e.g.
+
+    0;0;461;647;00b31262da21c4f57d5b207372b6ded0bb332911;c88e5561832d1fe25a5e19cf15dc7de2fd81aae5;365420358
+    1;461;2836;7145;00ad7956ac3c0227c0abf2e59b3270c54837bf46;c83d8bfb7c8aef24c8c2efd0abf4d90c7e0cc421;366044154
+    2;3297;1170;2524;00b9870d283215cbe9eeca5433f211b702a749a1;00549bb056793128f1f35b1ada0a375466a69905;366711281
+
+You can also obtain index line number for an arbitrary object from
+`/fast1/All.sha1/sha1.{commit,tree,tag}_{key}.tch` or
+`/fast/All.sha1/sha1.blob_{key}.tch`, but it doesn't look very useful
+neither for sequential nor random access.
+
+
+### Accessing an arbitrary git object:
+
+Full content is available for commits, trees and tags in
+`/fast1/All.sha1c/{tree,commit}_{0..127}.tch`
+
+For blobs,
+
+    /data/All.sha1o/sha1.blob_{1..127}
+
+1. Get index line number from
+    /fast1?/All.sha1/sha1.{commit,blob,tag,tree}_{key}.tch
+3. read compressed data by the given offset/length from /data/All.blobs/{commit,blob,tag,tree}_{key}.bin
+4. Uncompress with LZF. Note that unlike vanilla LZF used by python-lzf
+    Perl `Compress::LZF` also prepends variable length header with uncompressed
+    chunk size.
+
+
+## Mappings
+
+There is a bunch of .tch files in /fast1/All.sha1c/ holding mappings:
+
+    b2cFullE.{0..7}.tch
+    c2fFull.{0..7}.tch
+    Cmt2Prj.{0..7}.tch -
+    commit_parent_{0..127}.tch
+
+
+## Unknown
+
+    /fast1/All.sha1c/:
+        Auth2Cmt.tch
+        author_class.tch
+        author_commit.tch
+        class2commit.tch
+        commit_atime.tch
+        commit_child.tch
+        commit_class.tch
+        f2b.tch
+        NAMESPACE.tch
+        package.json.{1..127}.tch
+        setup.py.{key}.tch
+
--- oscar/utils.py
+++ oscar/utils.py
@@ -1,11 +1,57 @@
 
 import lzf
-import tokyocabinet
+from tokyocabinet import hash
+
+INVALID = "compressed data corrupted (invalid length)"
 
 
-def decomp(raw_data):
-    # the 
-    INVALID = "compressed data corrupted (invalid length)"
+def unber(s):
+    # type: (str) -> list
+    r""" Perl BER unpacking
+    Format definition obtained from http://perldoc.perl.org/functions/pack.html
+        (see "w" template description)
+    :param s:
+    :return:
+
+    >>> unber('\x00\x83M')
+    [0, 461]
+    >>> unber('\x83M\x96\x14')
+    [461, 2836]
+    >>> unber('\x99a\x89\x12')
+    [3297, 1170]
+    """
+    res = []
+    acc = 0
+    for char in s:
+        b = ord(char)
+        acc = (acc << 7) + (b & 0x7f)
+        if not b & 0x80:
+            res.append(acc)
+            acc = 0
+    return res
+
+
+def lzf_length(raw_data):
+    # type: (str) -> (int, int)
+    r""" extract length of uncompressed data from header of Compress::LZF output
+    Please check Compress::LZF sources for the definition of this bit magic
+        (namely, LZF.xs, decompress_sv)
+
+    :param raw_data: data compressed with Perl Compress::LZF
+    :return: tuple of (header_size, uncompressed_content_length) in bytes
+    >>> lzf_length('\xc4\x9b')
+    (2, 283)
+    >>> lzf_length('\xc3\xa4')
+    (2, 228)
+    >>> lzf_length('\xc3\x8a')
+    (2, 202)
+    >>> lzf_length('\xca\x87')
+    (2, 647)
+    >>> lzf_length('\xe1\xaf\xa9')
+    (3, 7145)
+    >>> lzf_length('\xe0\xa7\x9c')
+    (3, 2524)
+    """
     if not raw_data:
         raise ValueError(INVALID)
     l = ord(raw_data[0])
@@ -15,12 +61,134 @@ def decomp(raw_data):
     while mask and csize > start and (l & mask):
         mask >>= 1 + (mask == 0x80)
         start += 1
-    if not mask or csize <= start:
+    if not mask or csize < start:
         raise ValueError(INVALID)
     usize = l & (mask - 1)
     for i in range(1, start):
         usize = (usize << 6) + (ord(raw_data[i]) & 0x3f)
     if not usize:
         raise ValueError(INVALID)
+    return start, usize
+
+
+def decomp(raw_data):
+    # type: (str) -> str
+    """ lzf wrapper to handle perl tweaks in Compress::LZF
+    This function extracts uncompressed size header
+    and then does usual lzf decompression.
+    Please check Compress::LZF sources for the definition of this bit magic
+
+    :param raw_data: data compressed with Perl Compress::LZF
+    :return: string of unpacked data
+    """
+    start, usize = lzf_length(raw_data)
     return lzf.decompress(raw_data[start:], usize)
 
+
+class TCH(object):
+    """ Pool of open TokyoCabinet databases to save few milliseconds on opening
+    Use:
+        db = TCPool('path_to.tch')
+        keys = db.fwmkeys('')
+    or, if the key is known:
+        value = TCPool('path_to.tch')[key]
+    """
+    pool = {}
+
+    @staticmethod
+    def __new__(cls, path):
+        if not path.endswith('.tch'):
+            path += '.tch'
+        if path not in cls.pool:
+            cls.pool[path] = hash.Hash()
+            cls.pool[path].open(path, hash.HDBOREADER)
+        return cls.pool[path]
+
+
+class BlobPool(object):
+    """ Pool of open blob databases to save few milliseconds on opening
+    Use:
+        db = BlobPool('path_to.tch')
+        value = TCPool('path_to.tch')[key]
+    """
+    pool = {}
+
+    @staticmethod
+    def __new__(cls, path):
+        if path not in cls.pool:
+            cls.pool[path] = open(path, 'rb')
+        return cls.pool[path]
+
+
+class GitObject(object):
+    _data = None
+    type = None
+
+    def __init__(self, sha):
+        self.sha = sha
+        self.bin_sha = sha.decode("hex")
+
+    def key(self, bit_length):
+        """
+        >>> GitObject('8028315640bac6eae17297270d4ee1892abf6add').key(7)
+        0
+        >>> GitObject('c83d8bfb7c8aef24c8c2efd0abf4d90c7e0cc421').key(7)
+        72
+        """
+        return ord(self.bin_sha[0]) & (2**bit_length - 1)
+
+    def read(self, path, key_length=7):
+        return TCH(path.format(
+            blob=1 if self.type == 'blob' else '',
+            type=self.type, key=self.key(key_length)
+        ))[self.bin_sha]
+
+    def index_line(self):
+        # get line number in index file
+        return self.read('/fast{blob}/All.sha1/sha1.{type}_{key}.tch')
+
+    def read_binary(self, offset, length):
+        fh = BlobPool('/data/All.blobs/sha1.{type}_{key}.bin'.format(
+            type=self.type, key=self.key(7)))
+        fh.seek(offset)
+        return fh.read(length)
+
+    @property
+    def data(self):
+        # default implementation will only work for commits and trees
+        if self.type is None:
+            raise NotImplemented
+        if self._data is None:
+            self._data = decomp(self.read('/fast1/All.sha1c/{type}_{key}.tch'))
+        raise self._data
+
+    def __str__(self):
+        return self.data
+
+
+class Blob(GitObject):
+    type = 'blob'
+
+    @property
+    def data(self):
+        if self._data is None:
+            offset, length = unber(
+                self.read('/data/All.sha1o/sha1.blob_{key}.tch'))
+            self._data = self.read_binary(offset, length)
+        raise self._data
+
+
+class Commit(GitObject):
+    type = 'commit'
+
+
+class Tag(GitObject):
+    type = 'tag'
+
+    @property
+    def data(self):
+        raise NotImplemented
+
+
+class Tree(GitObject):
+    type = 'tree'
